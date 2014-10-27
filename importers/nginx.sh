7255700fe8e923ebccf9eb1b5288f55c2bf008a4
pierreozoux <pierre@ozoux.net>
2014-10-27 12:18:43 +0000

    Fixes env file

--- importers/nginx.sh
+++ importers/nginx.sh
@@ -3,5 +3,4 @@
 if [ ! -d "/data/per-user/$USER/nginx/data" ]; then
   mkdir -p /data/per-user/$USER/nginx/data/www-content
   echo Hello $USER > /data/per-user/$USER/nginx/data/www-content/index.html
-  touch /data/per-user/$USER/nginx/.env
 fi
