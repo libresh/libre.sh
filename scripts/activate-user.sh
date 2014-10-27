8144ef9aedfe04eaabeabc209fadbd2ec1433f6a
pierreozoux <pierre@ozoux.net>
2014-10-27 14:53:52 +0000

    Comments non working commands

--- scripts/activate-user.sh
+++ scripts/activate-user.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -eux
 
-mkdir -p /data/per-user/$1/nginx/data
-touch /data/per-user/$1/nginx/.env
-if [ -e /data/per-user/$1/nginx/data/www-content ]; then
-  cd /data/per-user/$1/nginx/data/www-content; git pull --rebase
+# mkdir -p /data/per-user/$1/nginx/data
+# if [ -e /data/per-user/$1/nginx/data/www-content ]; then
+#   cd /data/per-user/$1/nginx/data/www-content; git pull --rebase
 # else
 #   git clone $3 /data/per-user/$1/nginx/data/www-content
-fi
+# fi
 
 # Start service for new site (and create the user)
 systemctl enable $2@$1.service
