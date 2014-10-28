69ba357be0be1df33623428b52cd62a3d73dbbf5
Michiel de Jong <michiel@unhosted.org>
2014-10-28 17:04:11 +0000

    don't hardcode image to nginx in scripts/activate-user.sh

--- scripts/activate-user.sh
+++ scripts/activate-user.sh
@@ -4,13 +4,13 @@ if [ $# -ge 2 ]; then
   DOMAIN=$1
   IMAGE=$2
 else
-  echo "Usage: sh /data/indiehosters/scripts/activate-user domain image [gitrepo]"
+  echo "Usage: sh /data/indiehosters/scripts/activate-user.sh domain image [gitrepo]"
   exit 1
 fi
-mkdir -p /data/per-user/$DOMAIN/nginx/data
+mkdir -p /data/per-user/$DOMAIN/$IMAGE/data
 if [ $# -ge 3 ]; then
   GITREPO=$3
-  echo $GITREPO > /data/per-user/$DOMAIN/nginx/data/git-url.txt
+  echo $GITREPO > /data/per-user/$DOMAIN/$IMAGE/data/git-url.txt
 fi
 
 # Start service for new site (and create the user). This will also enable the git puller.
