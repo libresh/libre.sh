4beb94f5ec6c4eff14bcfd6e44760184de212fce
pierreozoux <pierre@ozoux.net>
2014-11-04 19:25:39 +0000

    Refactors directory structure

--- importers/mysql.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -eux
-
-if [ ! -d "/data/domains/$DOMAIN/mysql/data" ]; then
-  mkdir -p /data/domains/$DOMAIN/mysql/data
-  echo MYSQL_PASS=`echo $RANDOM  ${date} | md5sum | base64 | cut -c-10` > /data/domains/$DOMAIN/mysql/.env
-fi
--- importers/wordpress.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eux
-
-if [ ! -d "/data/domains/$DOMAIN/wordpress/data" ]; then
-  cd /data/domains/$DOMAIN/
-  tar xvzf /data/indiehosters/blueprints/wordpress.tgz
-fi
-
-cat /data/domains/$DOMAIN/mysql/.env | sed s/MYSQL_PASS/DB_PASS/ > /data/domains/$DOMAIN/wordpress/.env
