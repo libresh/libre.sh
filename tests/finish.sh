83f517048106d5c5a4f2daeae304a746cff3eb84
pierreozoux <pierre@ozoux.net>
2015-02-20 17:58:22 +0000

    Adds piwik and owncloud to tests

--- tests/finish.sh
+++ tests/finish.sh
@@ -2,7 +2,7 @@
 
 systemctl list-units | grep -c failed | grep 0
 
-applications=( static wordpress known )
+applications=( static wordpress known piwik owncloud )
 
 for application in "${applications[@]}"
 do
