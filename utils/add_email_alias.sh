915537e55cb6c5ab81ae5f957f6743fc192c5cbc
root <root@server.test>
2015-03-06 18:47:45 +0000

    wip

---
+++ utils/add_email_alias.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -ex
+
+DOMAIN=$1
+SOURCE=$2
+DESTINATION=$3
+MYSQL_PASS=`cat /data/domains/mail/mysql/.env | cut -d= -f2`
+
+/usr/bin/docker run \
+  --rm \
+  --name add_email_support_to_$DOMAIN \
+  --link mysql-mail:db \
+  pierreozoux/mysql \
+    mysql \
+      -uadmin \
+      -p$MYSQL_PASS \
+      -h db \
+        -e "INSERT INTO servermail.virtual_aliases (domain_id, source , destination) \
+          VALUES( \
+            (SELECT id FROM servermail.virtual_domains WHERE name='$DOMAIN'), \
+            '$SOURCE',
+            '$DESTINATION');"
+
